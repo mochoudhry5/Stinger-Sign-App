a563f8ede7edba57107c5317e92fd4e76b2e2710
Momin Choudhry <56105061+mochoudhry5@users.noreply.github.com>
2022-02-28 18:02:43 -0800

    Update ApolloSetup.js

--- src/Apollo-setup/ApolloSetup.js
+++ src/Apollo-setup/ApolloSetup.js
@@ -2,10 +2,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
 export default new ApolloClient({
-  uri: "https://yvgr2o4rfd.execute-api.us-west-2.amazonaws.com/graphql/",
+  uri: "ENTER_GRAPHQL_URL",
   cache: new InMemoryCache(),
   headers: {
     "Content-Type": "application/graphsql",
-    "X-API-KEY": "bycvrPPj_0fyVugRHMLfIJGexEXLNr9MYex1FDFp",
+    "X-API-KEY": "ENTER_API_KEY",
   },
 });
